bf17caf5487ee40458ae1a5684e7a2143bc4724a
YuChenLiao <yuchenliao3@gmail.com>
2021-04-15 11:00:31 +0800

    完成登录页所有样式

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Input } from 'antd';
-import { createFromIconfontCN } from '@ant-design/icons';
+import { createFromIconfontCN, EyeTwoTone, EyeInvisibleOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import './login.css';
 
@@ -15,6 +15,8 @@ class Login extends React.Component {
       prime: 'inline',
       login: 'none',
       register: 'none',
+      registerPrime: 'inline',
+      registerEnd: 'none'
     }
   }
   toLogin = () => {
@@ -27,6 +29,10 @@ class Login extends React.Component {
     this.setState({login: 'none'});
     this.setState({prime: 'none'});
     this.setState({register: 'inline'});
+  }
+  registerComplete = () => {
+    this.setState({registerPrime: 'none'});
+    this.setState({registerEnd: 'inline'});
   }
   render(){
     return (
@@ -54,53 +60,82 @@ class Login extends React.Component {
                 <Input 
                   bordered={false} 
                   size="large" 
+                  placeholder="账号:"
                   prefix={<IconFont type="icon-ic_user_platform" style={{fontSize: '25px',color: '#015266'}} />}
                 ></Input>
               </div>
               <div style={{borderBottomStyle: 'solid',borderBottomColor: '#CB5920',borderWidth: '2px',marginTop: '15px'}}>
-                <Input 
+                <Input.Password
                   bordered={false} 
                   size="large" 
+                  placeholder="密码:"
                   prefix={<IconFont type="icon-mimajiesuo" style={{fontSize: '25px',color: '#015266'}} />}
-                ></Input>
+                  iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                />
               </div>
               <div 
                 className="loginButton" 
                 style={{transform: ['translate(-50%,100%)'],color: '#FFFFFF',backgroundColor: '#015266'}}
               >登陆</div>
+              <div 
+                style={{
+                  color: '#015266',
+                  borderBottomStyle: 'solid',
+                  borderBottomColor: '#015266',
+                  borderWidth: '1px',
+                  position: 'absolute',
+                  bottom: '-120%',
+                  left: '50%',
+                  transform: ['translate(-50%,0)'],
+                  fontSize: '5px',
+                  whiteSpace: 'nowrap'
+                }}
+                onClick={this.toRrgister.bind()}
+              >还没有账号？这里注册</div>
             </div>
           </div>
         </div>
         <div className="register" style={{display:this.state.register}}>
-        <div className="primeFooter">
-            <div className="primeHeader2" style={{fontSize: '25px',top: '10%',color: '#015266',zIndex: '2'}}>新用户？你好！</div>
+          <div className="primeFooter" style={{display: this.state.registerPrime,transform:['translate(-50%,0%)']}}>
+            <div 
+              className="primeHeader2" 
+              style={{fontSize: '25px',top: '10%',color: '#015266',zIndex: '2'}}
+            >新用户？你好！</div>
             <div className="loginForm">
               <div style={{borderBottomStyle: 'solid',borderBottomColor: '#CB5920',borderWidth: '2px',marginTop: '15px'}}>
                 <Input 
                   bordered={false} 
                   size="large" 
+                  placeholder="用户名:"
                   prefix={<IconFont type="icon-ic_user_platform" style={{fontSize: '25px',color: '#015266'}} />}
                 ></Input>
               </div>
               <div style={{borderBottomStyle: 'solid',borderBottomColor: '#CB5920',borderWidth: '2px',marginTop: '15px'}}>
-                <Input 
+                <Input.Password
                   bordered={false} 
                   size="large" 
+                  placeholder="密码:"
                   prefix={<IconFont type="icon-mimajiesuo" style={{fontSize: '25px',color: '#015266'}} />}
-                ></Input>
+                />
               </div>
               <div style={{borderBottomStyle: 'solid',borderBottomColor: '#CB5920',borderWidth: '2px',marginTop: '15px'}}>
-                <Input 
+                <Input.Password 
                   bordered={false} 
                   size="large" 
+                  placeholder="重复密码:"
                   prefix={<IconFont type="icon-mimajiesuo" style={{fontSize: '25px',color: '#015266'}} />}
-                ></Input>
+                />
               </div>
               <div 
                 className="loginButton" 
                 style={{transform: ['translate(-50%,100%)'],color: '#FFFFFF',backgroundColor: '#015266'}}
+                onClick={this.registerComplete.bind()}
               >注册</div>
             </div>
+          </div>
+          <div className="registerEnd" style={{display: this.state.registerEnd}} onClick={this.toLogin.bind()}>
+            <div className="complete"></div>
+            <div style={{position: 'absolute',width:'100%',height: '30px',color: '#015266',top: '55%',fontSize: '25px'}}>注册完成</div>
           </div>
         </div>
       </div>
